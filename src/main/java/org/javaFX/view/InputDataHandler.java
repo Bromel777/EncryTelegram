8126e38c9023d6a9c21f5bb443622230d9a1efbc
Dmitriy Shabunevich <shabundzmy@gmail.com>
2020-05-19 18:56:34 +0300

    resetFields function

--- src/main/java/org/javaFX/view/InputDataHandler.java
+++ src/main/java/org/javaFX/view/InputDataHandler.java
@@ -3,6 +3,8 @@ package org.javaFX.view;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import org.javaFX.EncryWindow;
 
@@ -13,15 +15,32 @@ public class InputDataHandler {
     @FXML
     private TextField phoneNumber;
     @FXML
+    private ImageView resetPhoneNumberImage;
+    @FXML
     private TextField verificationCode;
     @FXML
     private TextField password;
     @FXML
     private Button confirmVCButton;
     @FXML
+    private ImageView resetVCImage;
+    @FXML
+    private ImageView resetPasswordImage;
+    @FXML
     private Button signInButton;
 
+    private final String imageCrossPath = "file:src/main/resources/images/simpleCross.png";
+
+
     public InputDataHandler() {
+        initImageViews();
+    }
+
+    private void initImageViews(){
+        Image crossImage = new Image(getClass().getResourceAsStream(imageCrossPath));
+        resetPhoneNumberImage = new ImageView(crossImage);
+        resetVCImage = new ImageView(crossImage);
+        resetPasswordImage = new ImageView(crossImage);
     }
 
     public void setStage(Stage stage) {
@@ -57,6 +76,28 @@ public class InputDataHandler {
         System.out.println("hello from sing in");
         String pass = password.getCharacters().toString();
         System.out.println(pass);
+    }
+
+    @FXML
+    public void resetPhoneNumberInputAction(){
+        System.out.println("hello from reset number");
+        phoneNumber.setText("");
+        resetVCImage.setVisible(true);
+        resetVCImage.setDisable(false);
+    }
+
+    @FXML
+    public void resetVCAction(){
+        System.out.println("hello from reset vc");
+        verificationCode.setText("");
+        resetPasswordImage.setVisible(true);
+        resetPasswordImage.setDisable(false);
+    }
+
+    @FXML
+    public void resetPasswordAction(){
+        System.out.println("hello from reset password");
+        password.setText("");
     }
 
 }

f23c54f49ddbaa91a541bb9a217dbc6421f2db6d
aleksandr <alexbromel@gmail.com>
2020-06-06 13:07:47 +0300

    code cleanup

--- src/main/scala/org/encryfoundation/tg/commands/PrintChats.scala
+++ src/main/scala/org/encryfoundation/tg/commands/PrintChats.scala
@@ -17,13 +17,11 @@ case class PrintChats[F[_]: Concurrent: Timer](client: Client[F],
   override val name: String = "showChats"
 
   override def run(args: List[String]): F[Unit] = for {
-    _ <- Sync[F].delay(println("init showChats"))
     state <- userStateRef.get
     _ <- if (state.isAuth && state.chatList.length < 20) client.send(
       new TdApi.GetChats(new TdApi.ChatListMain(), Long.MaxValue, 0, 20),
       DummyHandler[F](client, userStateRef, (cl, ref) => getChats(cl, ref))
     ) else Sync[F].delay(println("State auth - false"))
-    _ <- Sync[F].delay(println(s"trying to print chats. state.chatList.length: ${state.chatList.length}"))
     _ <- if (state.chatList.length < 20) run(args) else for {
       dbChats <- db.get(Database.privateGroupChatsKey)
       _ <- Sync[F].delay(println(s"Chats: ${

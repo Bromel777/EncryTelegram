88bbbe5c0ffe9951646424eea4ee1daa369587b8
Bromel777 <33941049+Bromel777@users.noreply.github.com>
2020-07-22 16:31:45 +0300

    Revert "fix (#24)" (#25)
    This reverts commit ad2a61ee62e2ab54b890187a8f18394d929b8d01.

--- src/main/scala/org/encryfoundation/tg/leveldb/Database.scala
+++ src/main/scala/org/encryfoundation/tg/leveldb/Database.scala
@@ -5,14 +5,13 @@ import java.io.File
 import cats.Applicative
 import cats.effect.{Resource, Sync}
 import cats.implicits._
-import org.iq80.leveldb.{DB, Options, ReadOptions}
+import org.iq80.leveldb.{DB, Options}
 import scorex.crypto.hash.Blake2b256
 
 trait Database[F[_]] {
   def put(key: Array[Byte], value: Array[Byte]): F[Unit]
   def get(key: Array[Byte]): F[Option[Array[Byte]]]
   def remove(key: Array[Byte]): F[Unit]
-  def getAllKeys(): F[List[Array[Byte]]]
 }
 
 object Database {
@@ -21,36 +20,14 @@ object Database {
 
   final private case class Live[F[_]: Sync](db: DB) extends Database[F] {
 
-    override def get(key: Array[Byte]): F[Option[Array[Byte]]] = Sync[F].delay {
-      val readOptions = new ReadOptions()
-      readOptions.snapshot(db.getSnapshot)
-      try {
-        val res = db.get(key)
-        if (res == null) Option.empty[Array[Byte]] else res.some
-      } finally readOptions.snapshot().close()
-    }
+    override def get(key: Array[Byte]): F[Option[Array[Byte]]] = Sync[F].delay{
+      val res = db.get(key)
+      if (res == null) Option.empty[Array[Byte]] else res.some
+      }
 
     override def put(key: Array[Byte], value: Array[Byte]): F[Unit] = Sync[F].delay(db.put(key, value))
 
     override def remove(key: Array[Byte]): F[Unit] = Sync[F].delay(db.delete(key))
-
-    override def getAllKeys(): F[List[Array[Byte]]] = Sync[F].delay {
-      val readOptions = new ReadOptions()
-      readOptions.snapshot(db.getSnapshot)
-      val iter = db.iterator(readOptions)
-      try {
-        iter.seekToFirst()
-        var buffer: List[Array[Byte]] = List.empty[Array[Byte]]
-        while (iter.hasNext) {
-          val nextKey = iter.next().getKey
-          buffer ::= nextKey
-        }
-        buffer
-      } finally {
-        iter.close()
-        readOptions.snapshot().close()
-      }
-    }
   }
 
   def apply[F[_]: Sync](dir: File): Resource[F, Database[F]] = {
